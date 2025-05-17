bfed54bb548bf9daa91db0d4dd9e547930512f06
zovalger <zovalger@gmail.com>
2025-05-17 10:00:57 -0400

    init

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { ScrollView, Text } from "react-native";
+
+export default function Index() {
+	return (
+		<ScrollView className="flex flex-row">
+			<Text className="border mt-3 ">
+				Edit sssapdassdap/index.tsx to edit this screen.
+			</Text>
+		</ScrollView>
+	);
+}
